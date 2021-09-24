02547f6f790f7de2d55603063dd2b6c2825edf04
Patel Kaushal <kordiyakaushal@gmail.com>
2021-09-24 12:53:28 +0530

    Feat: user register test cases

--- routes/users.js
+++ routes/users.js
@@ -6,23 +6,23 @@ const mongoose = require('mongoose');
 const { User, validate } = require('../models/user');
 const auth = require('../middleware/auth');
 
-router.get('/', async (req, res) => {
-    const users = await User.find().sort("name");
-    res.send(users);
-});
+// router.get('/', async (req, res) => {
+//     const users = await User.find().sort("name");
+//     res.send(users);
+// });
 
-router.get('/:id', async (req, res) => {
-    let user = await User.findById(req.params.id)
-    if (!user) return res.send(404, "Not found")
+// router.get('/:id', async (req, res) => {
+//     let user = await User.findById(req.params.id)
+//     if (!user) return res.send(404, "Not found")
 
-    res.send(user);
-});
+//     res.send(user);
+// });
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) {
-        res.send(400, error);
-        console.log(error);
+        res.status(400).send(error);
+        // console.log(error);
         return;
     }
     
@@ -41,28 +41,28 @@ router.post('/', async (req, res) => {
 
 });
 
-router.put('/:id', async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) {
-        res.send(400, error);
-        console.log(error);
-        return;
-    }
+// router.put('/:id', async (req, res) => {
+//     const { error } = validate(req.body);
+//     if (error) {
+//         res.send(400, error);
+//         console.log(error);
+//         return;
+//     }
 
-    let user = await User.findByIdAndUpdate(req.params.id, {name: req.body.name, email: req.body.email, password:req.body.password}, {new: true})
-    if (!user) return res.send(404, "Not found")
+//     let user = await User.findByIdAndUpdate(req.params.id, {name: req.body.name, email: req.body.email, password:req.body.password}, {new: true})
+//     if (!user) return res.send(404, "Not found")
 
-    return res.send(user);
+//     return res.send(user);
 
-})
+// })
 
-router.delete('/:id', async (req, res) => {
-    let user = await User.findByIdAndRemove(req.params.id)
+// router.delete('/:id', async (req, res) => {
+//     let user = await User.findByIdAndRemove(req.params.id)
 
-    if (!user) return res.send(404, "Not found")
+//     if (!user) return res.send(404, "Not found")
 
-    return res.send(user);
-});
+//     return res.send(user);
+// });
 
 
 module.exports = router;
---
+++ tests/integration/user.test.js
@@ -0,0 +1,74 @@
+const { User } = require('../../models/user');
+const request = require('supertest');
+
+let server;
+
+describe('user register', () => {
+    let email;
+    let name;
+    let password;
+
+    beforeEach(() => { 
+        server = require('../../index');
+        email = "test@gmail.com"
+        name = "abcdefg"
+        password = "abcdefghijkl"
+    });
+
+    afterEach(async () => { 
+        await server.close();
+        await User.deleteMany({});
+    });
+    
+    const exec = () => {
+        return request(server).post('/api/users').send({name: name, email: email, password: password})
+    }
+
+    it('should return 400 if email is not provided', async () => {
+        email = '';
+        const res = await exec();
+    
+        expect(res.status).toBe(400);
+
+    });
+
+    it('should return 400 if password is not provided', async () => {
+        password = '';
+        const res = await exec();
+    
+        expect(res.status).toBe(400);
+
+    });
+
+    it('should return 400 if name is not provided', async () => {
+        name = '';
+        const res = await exec();
+        expect(res.status).toBe(400);
+
+    });
+
+    it('should return 400 if email is already registered', async () => {
+        user = new User({email: email, name: name, password: password})
+        await user.save()
+
+        const res = await exec();
+    
+        expect(res.status).toBe(400);
+
+    });
+
+    it('should return 200 if user input is valid', async () => {
+        const res = await exec();
+    
+        expect(res.status).toBe(200);
+
+    });
+
+    it('should return user payload with id', async () => {
+        const res = await exec();
+    
+        expect(res.body._id).toBeDefined();
+
+    });
+
+});
